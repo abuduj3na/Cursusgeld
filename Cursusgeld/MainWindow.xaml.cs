f72602dbb600b1711befdeab580f9e589148f7e3
MericUcarPXL <meric.ucar@icloud.com>
2024-10-21 23:31:40 +0200

    klaar

--- Cursusgeld/MainWindow.xaml.cs
+++ Cursusgeld/MainWindow.xaml.cs
@@ -20,5 +20,49 @@ namespace Cursusgeld
         {
             InitializeComponent();
         }
+
+        double pricePerHour = 1.5;
+        double hours;
+        double money = 0;
+        double year;
+        bool isNumeriek1;
+        bool isNumeriek2;
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            money = hours * pricePerHour;
+            if (year % 4 == 0)
+            {
+                money += pricePerHour;
+                schrikkeljaar.Content = "Is een schrikkeljaar";
+
+            }
+            else
+            {
+                schrikkeljaar.Content = "Is geen schrikkeljaar";
+            }
+
+            moneyTextBox.Text = money.ToString("C");
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void testButton_Click(object sender, RoutedEventArgs e)
+        {
+            isNumeriek1 = double.TryParse(lesHoursTextBox.Text, out hours);
+            isNumeriek2 = double.TryParse(yearTextBox.Text, out year);
+            if (isNumeriek1 && isNumeriek2)
+            {
+                calculateButton.IsEnabled = true;
+                numeriek.Content = "is numeriek";
+            }
+            else
+            {
+                calculateButton.IsEnabled = false;
+                numeriek.Content = "is niet numeriek";
+            }
+        }
     }
 }
